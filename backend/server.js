7e6ef578057f8ee35ac89ccfda7892dc06fe9e2a
Mallikarjuna-dev <76250603+Mallikarjuna-dev@users.noreply.github.com>
2024-07-26 18:59:22 +0530

    updates server code

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,17 @@ const cors = require("cors");
 const http = require("http");
 const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
-const { addTask } = require("./controllers/taskController");
+// const { addTask } = require("./controllers/taskController");
 const taskRoutes = require("./routes/taskRoutes");
-require("./configs/config")
+const { redisClient } = require("./configs/config");
+const Task = require("./models/taskModel");
 
 require("dotenv").config();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', taskRoutes);
+// app.use('/api', taskRoutes);
 
 const server = http.createServer(app);
 
@@ -24,11 +25,48 @@ const io = new Server(server, {
     }
 });
 
+// controllers code for functions
+const TASK_KEY = 'FULLSTACK_TASK_MALLIKARJUNA';
+let tasks = [];
+
+async function loadTasksFromRedis() {
+    const tasksData = await redisClient.get(TASK_KEY);
+    tasks = tasksData ? JSON.parse(tasksData) : [];
+}
+
+loadTasksFromRedis();
+
+async function saveTasksToRedis() {
+    await redisClient.set(TASK_KEY, JSON.stringify(tasks));
+    console.log('Tasks saved to Redis:', tasks);
+}
+
+async function moveTasksToMongoDB() {
+    if (tasks.length >= 50) {
+        console.log('Moving tasks to MongoDB:', tasks);
+        try {
+            await Task.insertMany(tasks);
+            tasks = [];
+            await redisClient.del(TASK_KEY);
+            console.log('Tasks moved to MongoDB and Redis cache cleared');
+        } catch (error) {
+            console.error('Error moving tasks to MongoDB:', error);
+        }
+    }
+}
+
+app.get('/fetchAllTasks', (req, res) => {
+    res.json(tasks);
+});
+
 io.on('connection', (socket) => {
     console.log('a user connected');
 
     socket.on('add', async (task) => {
-        await addTask(task);
+        tasks.push(task);
+        // await addTask(task);
+        await saveTasksToRedis();
+        await moveTasksToMongoDB();
         io.emit('taskAdded', task);
     });
 
